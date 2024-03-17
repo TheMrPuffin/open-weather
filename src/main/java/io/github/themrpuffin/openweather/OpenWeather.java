d0c2313dc0927be49e92e02834a8f986bd8a0092
TheMrPuffin <garnera99@gmail.com>
2024-03-17 16:59:05 +0000

    chore: Add initial Minecraft Forge mod template

---
+++ src/main/java/io/github/themrpuffin/openweather/OpenWeather.java
@@ -0,0 +1,8 @@
+package io.github.themrpuffin.openweather;
+
+import net.minecraftforge.fml.common.Mod;
+
+@Mod(OpenWeather.MODID)
+public class OpenWeather {
+    public static final String MODID = "openweather";
+}
